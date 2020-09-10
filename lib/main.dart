ca7556c19d36aac1a472a57d090a5a87e34ba56c
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-09-10 14:04:13 +0700

    pushReplacement-> intent and finish push-> intent not finish pop-> get page before intent(push)

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,13 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamental_yt/ui/LoginPage.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Color color1 = Colors.red;
-  Color color2 = Colors.green;
-  Color targetColor;
-
-  bool isAccepted = false;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar:
-            AppBar(title: Text("Draggable, DragTarget, SizedBox, Material")),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Draggable<Color>(
-                  data: color1,
-                  child: MySizeBox(color1: color1),
-                  childWhenDragging: MySizeBox(color1: Colors.black26),
-                  feedback: MySizeBox(color1: color1.withOpacity(0.7)),
-                ),
-                Draggable<Color>(
-                  data: color2,
-                  child: MySizeBox(color1: color2),
-                  childWhenDragging: MySizeBox(color1: Colors.black26),
-                  feedback: MySizeBox(color1: color2.withOpacity(0.7)),
-                ),
-              ],
-            ),
-            DragTarget<Color>(
-              onWillAccept: (value) => true,
-              onAccept: (value) {
-                isAccepted = true;
-                targetColor = value;
-              },
-              builder: (context, candidates, rejected){
-                return(isAccepted) ? MySizeBox(color1: targetColor) : MySizeBox(color1: Colors.black);
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MySizeBox extends StatelessWidget {
-  const MySizeBox({
-    Key key,
-    @required this.color1,
-  }) : super(key: key);
-
-  final Color color1;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 50,
-      height: 50,
-      child: Material(
-        color: color1,
-        shape: StadiumBorder(),
-        elevation: 3,
-      ),
+      home: LoginPage(),
     );
   }
 }
