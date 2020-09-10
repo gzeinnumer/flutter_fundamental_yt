f1d3821539d8ca373d4075fc795620f44c943bd3
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-09-10 12:36:33 +0700

    Stack, seperti coordinator rumus nya disini adalah. yang atas akan ada ditampilkan paling awal, makin ke bawah maka makin di atas lah tamplannya,
    Align -> rumus Alignment(x,y)
    x -> -1 , 0 , 1
    y -> -1
          0
          1

--- lib/main.dart
+++ lib/main.dart
@@ -10,50 +10,113 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Flexible Layout"),
+          title: Text("Latihan Stack dan AlignWidget"),
         ),
-        body: Column(
-          children: [
-            Flexible(
-              flex: 1,
-              child: Row(
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.red,
-                    ),
+        body: Stack(
+          children: <Widget>[
+            Column(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.white),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.black12),
+                      ),
+                    ],
                   ),
-                  Flexible(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.yellow,
-                    ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Flexible(
+                          flex: 1, child: Container(color: Colors.black12)),
+                      Flexible(flex: 1, child: Container(color: Colors.white)),
+                    ],
                   ),
-                  Flexible(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.lightBlue,
-                    ),
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
-            Flexible(
-              flex: 2,
-              child: Container(
+            ListView(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini adalah text yang ada ditengah dari lapisan stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini adalah text yang ada ditengah dari lapisan stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini adalah text yang ada ditengah dari lapisan stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini adalah text yang ada ditengah dari lapisan stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini adalah text yang ada ditengah dari lapisan stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini adalah text yang ada ditengah dari lapisan stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini adalah text yang ada ditengah dari lapisan stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini adalah text yang ada ditengah dari lapisan stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment(0.80, 0.95),
+              child: RaisedButton(
+                child: Text("MyButton"),
                 color: Colors.amber,
+                onPressed: () {},
               ),
             ),
-            Flexible(
-              flex: 1,
-              child: Container(
-                color: Colors.green,
-              ),
-            )
           ],
         ),
       ),
