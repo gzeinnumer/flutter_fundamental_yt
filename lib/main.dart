1b91a96f6d39185b43bd8ca278bdab4c4c61a6a9
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-09-10 18:49:14 +0700

    texinput layout

--- lib/main.dart
+++ lib/main.dart
@@ -8,39 +8,32 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  TextEditingController cont = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(backgroundColor: Colors.blueAccent,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan TextField"),
+        ),
         body: Container(
-          margin: EdgeInsets.all(10),
-          child: ListView(
+          margin: EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              buildCard(Icons.adb, "Text1"),
-              buildCard(Icons.account_box, "Text2"),
-              buildCard(Icons.repeat, "Text3"),
-              buildCard(Icons.exit_to_app, "Text4"),
-              buildCard(Icons.add, "Text5"),
-              buildCard(Icons.ac_unit, "Text6"),
-              buildCard(Icons.accessibility_new, "Text7"),
+              TextField(
+                maxLength: 20,
+                //obscureText: true,
+                onChanged: (value) {
+                  setState(() {});
+                },
+                controller: cont,
+              ),
+              Text(cont.text),
             ],
           ),
         ),
-      ),
-    );
-  }
-
-  Card buildCard(IconData iconData, String text) {
-    return Card(
-      elevation: 5,
-      child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.all(5),
-            child: Icon(iconData),
-          ),
-          Text(text)
-        ],
       ),
     );
   }
