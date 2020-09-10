a16dfd9a6b98359b9283c3ee6cedec5d2c82248f
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-09-10 14:46:01 +0700

    App Bar Style leading -> icon pada toolbar action -> sama seperi java flexiblespace -> memberi gradient dan gambar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_fundamental_yt/ui/LoginPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +6,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(
+            Icons.adb,
+            color: Colors.white,
+          ),
+          title: Text(
+            "AppBar Gradasi Dengan Corak",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {},
+            )
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    Color(0xff009677),
+                    Color(0xff6610f2),
+                  ],
+                  begin: FractionalOffset.topLeft,
+                  end: FractionalOffset.bottomRight),
+              image: DecorationImage(
+                image: AssetImage("images/bg.png"),
+                fit: BoxFit.none,
+                repeat: ImageRepeat.repeat,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
