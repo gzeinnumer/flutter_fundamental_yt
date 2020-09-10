87f1fb0cf12aaa32a94367fc73049fcfa84aaec4
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-09-10 18:47:57 +0700

    cardview

--- lib/main.dart
+++ lib/main.dart
@@ -8,32 +8,40 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  TextEditingController cont = TextEditingController();
-
+  TextEditingController c
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan TextField"),
-        ),
+      home: Scaffold(backgroundColor: Colors.blueAccent,
         body: Container(
-          margin: EdgeInsets.all(30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          margin: EdgeInsets.all(10),
+          child: ListView(
             children: [
-              TextField(
-                maxLength: 20,
-                //obscureText: true,
-                onChanged: (value) {
-                  setState(() {});
-                },
-                controller: cont,
-              ),
-              Text(cont.text),
+              buildCard(Icons.adb, "Text1"),
+              buildCard(Icons.account_box, "Text2"),
+              buildCard(Icons.repeat, "Text3"),
+              buildCard(Icons.exit_to_app, "Text4"),
+              buildCard(Icons.add, "Text5"),
+              buildCard(Icons.ac_unit, "Text6"),
+              buildCard(Icons.accessibility_new, "Text7"),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Card buildCard(IconData iconData, String text) {
+    return Card(
+      elevation: 5,
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.all(5),
+            child: Icon(iconData),
+          ),
+          Text(text)
+        ],
       ),
     );
   }
