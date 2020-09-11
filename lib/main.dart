575d3235f06c533187072b9b0a5df4780b3679c9
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-09-11 13:03:19 +0700

    media query, untuk mengecek apakah layar potrait atau lanscape, seperti bootstrap yang reponsive

--- lib/main.dart
+++ lib/main.dart
@@ -2,49 +2,66 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Latihan TextField Style"),
-          ),
-          body: MainPage()),
+        appBar: AppBar(
+          title: Text("Laithan Inkwell MyOwnButton"),
+        ),
+        body: Center(
+          child: getHome(),
+        ),
+      ),
     );
   }
-}
 
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return (MediaQuery.of(context).orientation == Orientation.portrait)
-        ? Column(children: generateContainer())
-        : Row(children: generateContainer());
-  }
-
-  List<Widget> generateContainer() {
-    return [
-      Container(
-        color: Colors.red,
-        width: 100,
-        height: 100,
-      ),
-      Container(
-        color: Colors.yellow,
-        width: 100,
-        height: 100,
-      ),
-      Container(
-        color: Colors.purple,
-        width: 100,
-        height: 100,
-      ),
-    ];
+  Column getHome() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        RaisedButton(
+          color: Colors.red,
+          child: Text("Raised Button"),
+          shape: StadiumBorder(),
+          onPressed: () {},
+        ),
+        Material(
+          borderRadius: BorderRadius.circular(20),
+          elevation: 2,
+          child: Container(
+            width: 150,
+            height: 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                colors: [Colors.purple, Colors.pink],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+            child: Material(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.transparent,
+              child: InkWell(
+                splashColor: Colors.amber,
+                borderRadius: BorderRadius.circular(20),
+                onTap: () {},
+                child: Center(
+                  child: Text(
+                    "My Button",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
