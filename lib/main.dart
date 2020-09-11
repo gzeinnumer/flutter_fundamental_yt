6aa315a21531e1511af310b8f548fc07513166db
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-09-11 12:46:54 +0700

    media query, untuk mengecek apakah layar potrait atau lanscape, seperti bootstrap yang reponsive

--- lib/main.dart
+++ lib/main.dart
@@ -12,88 +12,39 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan TextField Style"),
-        ),
-        body: Container(
-          margin: EdgeInsets.all(30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextField(
-                maxLength: 20,
-                decoration: InputDecoration(
-                  fillColor: Colors.black12,
-                  filled: true,
-                  icon: Icon(Icons.adb),
-                  prefix: Container(
-                    width: 5,
-                    height: 5,
-                    color: Colors.red,
-                  ),
-                  //1
-                  prefixIcon: Icon(
-                    Icons.person,
-                    color: Colors.red,
-                  ),
-                  prefixStyle: TextStyle(
-                    color: Colors.blueAccent,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  labelText: "labelText",
-                  labelStyle: TextStyle(
-                    color: Colors.blueAccent,
-                  ),
-                  hintText: "hintText",
-                  hintStyle: TextStyle(
-                    fontSize: 12,
-                  ),
-                  suffix: Container(
-                    width: 5,
-                    height: 5,
-                    color: Colors.red,
-                  ),
-                  //1
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              TextField(
-                maxLength: 20,
-                decoration: InputDecoration(
-                  fillColor: Colors.black12,
-                  filled: true,
-                  icon: Icon(Icons.adb),
-                  prefixText: "prefixText : ",
-                  //2 //pilih salah satu
-                  prefixIcon: Icon(
-                    Icons.person,
-                    color: Colors.red,
-                  ),
-                  prefixStyle: TextStyle(
-                    color: Colors.blueAccent,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  labelText: "labelText",
-                  labelStyle: TextStyle(
-                    color: Colors.blueAccent,
-                  ),
-                  hintText: "hintText",
-                  hintStyle: TextStyle(
-                    fontSize: 12,
-                  ),
-                  suffixText: "suffixText : ",
-                  //2 //pilih salah satu
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ],
+          appBar: AppBar(
+            title: Text("Latihan TextField Style"),
           ),
-        ),
-      ),
+          body: MainPage()),
     );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return (MediaQuery.of(context).orientation == Orientation.portrait)
+        ? Column(children: generateContainer())
+        : Row(children: generateContainer());
+  }
+
+  List<Widget> generateContainer() {
+    return [
+      Container(
+        color: Colors.red,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.yellow,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.purple,
+        width: 100,
+        height: 100,
+      ),
+    ];
   }
 }
