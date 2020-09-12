145cad04efa858e9b19e04383f7598e3d4815452
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-09-13 02:34:39 +0700

    custom height appbar

--- lib/main.dart
+++ lib/main.dart
@@ -7,133 +7,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Custome Car Example",
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Color(0xFF8C062F),
-      ),
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Color(0xFFFE5788), Color(0xFFF56D5D)],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter)),
-          ),
-          Center(
-            child: Container(
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                height: MediaQuery.of(context).size.height * 0.7,
-                child: Card(
-                  elevation: 10,
-                  child: Stack(
-                    children: [
-                      Opacity(
-                        opacity: 0.7,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            image: DecorationImage(
-                              image: NetworkImage(
-                                  "https://www.toptal.com/designers/subtlepatterns/patterns/memphis-mini.png"),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.35,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(4),
-                            topRight: Radius.circular(4),
-                          ),
-                          image: DecorationImage(
-                            image: NetworkImage(
-                                "https://cdn.pixabay.com/photo/2018/01/20/08/33/sunset-3094078_960_720.jpg"),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(
-                          20,
-                          50 + MediaQuery.of(context).size.height * 0.35,
-                          20,
-                          20,
-                        ),
-                        child: Center(
-                          child: Column(
-                            children: [
-                              Text(
-                                "Indahnya pemandangan",
-                                style: TextStyle(
-                                  color: Color(0xFFF56D5D),
-                                  fontSize: 25,
-                                ),
-                                maxLines: 2,
-                                textAlign: TextAlign.center,
-                              ),
-                              Container(
-                                margin: EdgeInsets.fromLTRB(0, 20, 0, 15),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      "Posted on ",
-                                      style: TextStyle(
-                                        color: Colors.grey,
-                                        fontSize: 12,
-                                      ),
-                                    ),
-                                    Text(
-                                      "June 18, 2019 ",
-                                      style: TextStyle(
-                                        color: Color(0xFFF56D5D),
-                                        fontSize: 12,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Spacer(flex: 10,),
-                                  Icon(Icons.thumb_up, size: 18, color: Colors.grey,),
-                                  Spacer(flex: 1,),
-                                  Text("99", style: TextStyle(color: Colors.grey),),
-                                  Spacer(flex: 5,),
-                                  Icon(Icons.comment, size: 18, color: Colors.grey,),
-                                  Spacer(flex: 1,),
-                                  Text("888", style: TextStyle(color: Colors.grey),),
-                                  Spacer(flex: 10,),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(200),
+          child: AppBar(
+            backgroundColor: Colors.green,
+            flexibleSpace: Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                margin: EdgeInsets.all(20),
+                child: Text(
+                  "AppBart with Custom Height",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
               ),
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
